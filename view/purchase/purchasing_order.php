eb06c5f0745c957ec1894340b09e10fb5b20ee32
hari-sathish <sathish@fixnix.co>
2019-01-11 13:43:09 +0530

    pawn_purchase

--- view/purchase/purchasing_order.php
+++ view/purchase/purchasing_order.php
@@ -64,7 +64,7 @@
     <div class="page-content-wrapper">      <!-- BEGIN CONTENT BODY -->
       <div class="page-content" >
         <div class="col-md-12">
-           <div class="row" style="margin-left:3px;margin-bottom:10px;">
+           <div class="row" style="margin-left:1px;margin-bottom:10px;">
     <div class="clearfix" style="float: left;">   
 
              <button type="button" class="btn btn-success" onclick="location.href='view/interest/interest.php'">Interest</button>       
